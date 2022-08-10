c8b8ec3e0595f22200b2cd5b298016dfd1945dbd
ahmedadel56 <ahmdadl563@gmail.com>
2022-08-10 22:07:09 +0200

    Add react redux linters

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <header className="App-header">
+        {' '}
+        <p>
+          Edit
+          {' '}
+          <code>src/App.js</code>
+          {' '}
+          and save to reload.
         </p>
         <a
           className="App-link"
