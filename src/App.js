35fc7d2a5fa069fc9f3a4bb7d427a85458ee02f4
ahmedadel56 <ahmdadl563@gmail.com>
2022-08-13 01:00:27 +0200

    Add Redux reducers

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { useDispatch } from 'react-redux';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import { fetchMostRecentHotels } from './redux/MostRecent/MostRecent';
+import SignUp from './components/SignUp';
+import Login from './components/Login';
 
 const App = () => {
   const dispatch = useDispatch();
@@ -16,6 +18,8 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="sign-up" element={<SignUp />} />
+        <Route path="log-in" element={<Login />} />
       </Routes>
     </Router>
   );
--- src/redux/User/User.js
+++ src/redux/User/User.js
@@ -1,4 +1,6 @@
 const CREATE_USER = 'HotelAgentFrontEnd/User/CREATE_USER';
+const LOGIN_USER = 'HotelAgentFrontEnd/User/LOGIN_USER';
+
 const initialState = { status: 'Not created', data: [] };
 
 export const success = (user) => ({
@@ -6,7 +8,18 @@ export const success = (user) => ({
   user,
 });
 
-export const createUser = () => async (dispatch) => {
+export const loginSuccess = (user) => ({
+  type: LOGIN_USER,
+  user,
+});
+
+const setToken = (token) => {
+  localStorage.setItem('token', token);
+  localStorage.setItem('lastLoginTime', new Date(Date.now()).getTime());
+  console.log(localStorage.getItem('token'));
+};
+
+export const createUser = (data) => async (dispatch) => {
   fetch('http://localhost:3000/v1/users', {
     method: 'POST',
     headers: {
@@ -14,14 +27,39 @@ export const createUser = () => async (dispatch) => {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      name: 'Augusto',
-      email: 'auguto@hotmail.com',
-      password: '123456',
+      name: data.name,
+      email: data.email,
+      password: data.password,
     }),
   })
     .then((res) => res.json())
     .then((res) => {
       dispatch(success(res));
+      if (res.ok) {
+        setToken(res.content.auth_token);
+      }
+    })
+    .catch((error) => { throw error; });
+};
+
+export const logInUser = (data) => async (dispatch) => {
+  fetch('http://127.0.0.1:3000/v1/auth/login', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      email: data.email,
+      password: data.password,
+    }),
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      dispatch(loginSuccess(res));
+      if (res.ok) {
+        setToken(res.content.auth_token);
+      }
     })
     .catch((error) => { throw error; });
 };
@@ -30,7 +68,8 @@ const UserReducer = (state = initialState, action) => {
   switch (action.type) {
     case CREATE_USER:
       return { data: action.user, status: 'Created successfully' };
-
+    case LOGIN_USER:
+      return { data: action.user, status: 'Loged In successfully' };
     default:
       return state;
   }
