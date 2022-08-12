ea65c1d44f92e28325ad4342cc6da50e5874e0e0
Alexr16 <j.alex-1996@hotmail.com>
2022-08-12 15:04:15 -0500

    Implement dispatch actions

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import './App.scss';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import { fetchMostRecentHotels } from './redux/MostRecent/MostRecent';
 
-const App = () => (
-  <Router>
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-    </Routes>
-  </Router>
-);
+const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchMostRecentHotels());
+  });
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { createUser } from '../redux/User/User';
 
-const HomePage = () => (
-  <div className="App">
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  </div>
-);
+const HomePage = () => {
+  const dispatch = useDispatch();
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <button type="button" className="microphone" label="microphone" onClick={() => dispatch(createUser())}>create user</button>
+    </div>
+  );
+};
 
 export default HomePage;
