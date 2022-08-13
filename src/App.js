e1d3627ddbdea7225558bc4691608da845f93f77
Alexr16 <j.alex-1996@hotmail.com>
2022-08-13 08:20:32 -0500

    Add navbar to the header

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import NavBar from './components/Navbar';
 import { fetchMostRecentHotels } from './redux/MostRecent/MostRecent';
+import DetailsPage from './pages/DetailsPage';
 
 const App = () => {
   const dispatch = useDispatch();
@@ -14,8 +16,10 @@ const App = () => {
 
   return (
     <Router>
+      <NavBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/hotel-details" element={<DetailsPage />} />
       </Routes>
     </Router>
   );
