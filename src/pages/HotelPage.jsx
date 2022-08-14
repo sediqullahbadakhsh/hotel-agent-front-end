5928f8e4cee5e2cb068b2eef3bbc023297a1bd63
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-08-14 21:55:17 +0430

    create basic view to interact with Reducers

---
+++ src/pages/HotelPage.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { listHotel, addHotel } from '../redux/Hotel/Hotel';
+
+const HomePage = () => {
+  const dispatch = useDispatch();
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button
+        type="button"
+        className="microphone"
+        label="microphone"
+        onClick={() => dispatch(listHotel())}
+      >
+        List Hotel
+      </button>
+
+      <button
+        type="button"
+        className="microphone"
+        label="microphone"
+        onClick={() => dispatch(addHotel())}
+      >
+        Add Hotel
+      </button>
+    </div>
+  );
+};
+
+export default HomePage;
