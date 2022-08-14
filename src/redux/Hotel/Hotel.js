87a6b6ddc6ce9777fd85591907ba239c8e93cebc
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-08-14 21:54:44 +0430

    create listHotel reducer and action

--- src/redux/Hotel/Hotel.js
+++ src/redux/Hotel/Hotel.js
@@ -1,9 +1,14 @@
 const LIST_HOTEL = 'HotelAgentFrontEnd/Hotel/LIST_HOTEL';
+const ADD_HOTEL = 'HotelAgentFrontEnd/Hotel/ADD_HOTEL';
 
 const initialState = { status: 'No Data', data: [] };
 
 export const dataSucces = (hotel) => ({
   type: LIST_HOTEL,
+  hotel,
+});
+export const AddHotelSucces = (hotel) => ({
+  type: ADD_HOTEL,
   hotel,
 });
 
@@ -26,11 +31,37 @@ export const listHotel = () => async (dispatch) => {
     });
 };
 
+export const addHotel = () => async (dispatch) => {
+  fetch('http://127.0.0.1:3000/v1/users/2/hotels', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+    },
+    body: JSON.stringify({
+      name: 'Apple',
+      description: 'some description',
+      cost: 3.2,
+      address: 'somewhere',
+      image: 'some photo',
+    }),
+  })
+    .then((data) => data.json())
+    .then((data) => {
+      dispatch(AddHotelSucces(data));
+    })
+    .catch((error) => {
+      throw error;
+    });
+};
+
 const HotelReducer = (state = initialState, action) => {
   switch (action.type) {
     case LIST_HOTEL:
       return { data: action.hotel, status: 'successfully loaded' };
-
+    case ADD_HOTEL:
+      return { data: action.hotel, status: 'Hotel successfully added' };
     default:
       return state;
   }
