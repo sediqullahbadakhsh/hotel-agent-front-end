da5c926ed7fb7df9b90ff08c83213581fdbb1303
Alexr16 <j.alex-1996@hotmail.com>
2022-08-15 09:44:11 -0500

    Create social media components

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
-import { FiGithub, FiLinkedin, FiTwitter } from 'react-icons/fi';
-import { FaAngellist, FaMediumM } from 'react-icons/fa';
+import { IoLogoTwitter } from 'react-icons/io';
+import { FaVimeoV, FaPinterestP } from 'react-icons/fa';
+import { GrFacebookOption } from 'react-icons/gr';
+import { TiSocialGooglePlus } from 'react-icons/ti';
 
 const Social = () => (
   <ul className="icons">
     <li key={1} className="social-icons">
-      <button type="button" className="github social-button" label="GitHub profile"><a href="https://github.com/Alexr16" aria-label="Jorge's github account" target="_blank" rel="noreferrer noopener"><FiGithub /></a></button>
+      <button type="button" className="twitter social-button" label="Twitter profile"><a href="https://twitter.com/jorgeriosr16" aria-label="Twitter account" target="_blank" rel="noreferrer noopener"><IoLogoTwitter /></a></button>
     </li>
     <li key={2} className="social-icons">
-      <button type="button" className="linkdin social-button" label="Linkedin profile"><a href="https://www.linkedin.com/in/jorgeriosr/" aria-label="Jorge's linkedin account" target="_blank" rel="noreferrer noopener"><FiLinkedin /></a></button>
+      <button type="button" className="facebook social-button" label="facebook profile"><a href="https://www.linkedin.com/in/jorgeriosr/" aria-label="Facebook account" target="_blank" rel="noreferrer noopener"><GrFacebookOption /></a></button>
     </li>
     <li key={3} className="social-icons">
-      <button type="button" className="angellist social-button" label="AngelList profile"><a href="https://angel.co/u/jorge-rios-7" aria-label="Jorge's angellist account" target="_blank" rel="noreferrer noopener"><FaAngellist /></a></button>
+      <button type="button" className="google social-button" label="google profile"><a href="https://angel.co/u/jorge-rios-7" aria-label="Google account" target="_blank" rel="noreferrer noopener"><TiSocialGooglePlus /></a></button>
     </li>
     <li key={4} className="social-icons">
-      <button type="button" className="twitter social-button" label="Twitter profile"><a href="https://twitter.com/jorgeriosr16" aria-label="Jorge's twitter account" target="_blank" rel="noreferrer noopener"><FiTwitter /></a></button>
+      <button type="button" className="vimeo social-button" label="Vimeo profile"><a href="https://github.com/Alexr16" aria-label="Vimeo account" target="_blank" rel="noreferrer noopener"><FaVimeoV /></a></button>
     </li>
     <li key={5} className="social-icons">
-      <button type="button" className="medium social-button" label="Medium profile"><a href="https://medium.com/@jorge19960316" aria-label="Jorge's medium account" target="_blank" rel="noreferrer noopener"><FaMediumM /></a></button>
+      <button type="button" className="pinterest social-button" label="Pinterest profile"><a href="https://medium.com/@jorge19960316" aria-label="Pinterest account" target="_blank" rel="noreferrer noopener"><FaPinterestP /></a></button>
     </li>
   </ul>
 );
