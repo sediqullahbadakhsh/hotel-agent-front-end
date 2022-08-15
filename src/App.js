ccef36d4388c0e84c8d5e7e2b72331f319ccaa8d
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-08-16 02:45:03 +0430

    create featured page route

--- src/App.js
+++ src/App.js
@@ -5,19 +5,21 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import NavBar from './components/Navbar';
 import { fetchMostRecentHotels } from './redux/MostRecent/MostRecent';
+import FeaturedPage from './pages/FeaturedPage';
 
 const App = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchMostRecentHotels());
-  });
+  }, []);
 
   return (
     <Router>
       <NavBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/featured" element={<FeaturedPage />} />
       </Routes>
     </Router>
   );
