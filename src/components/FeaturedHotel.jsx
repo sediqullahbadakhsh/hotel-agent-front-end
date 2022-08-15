8ccb2df1bda65eed93e44fb98370fcedab30f84b
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-08-16 02:55:51 +0430

    add icons to carousel

--- src/components/FeaturedHotel.jsx
+++ src/components/FeaturedHotel.jsx
@@ -1,5 +1,7 @@
 /* eslint-disable */
 import React from 'react';
+import { RiFacebookCircleLine, RiInstagramLine } from 'react-icons/ri';
+import { TiSocialTwitterCircular } from 'react-icons/ti';
 
 const FeaturedHotel = ({ hotel }) => {
   return (
@@ -7,6 +9,17 @@ const FeaturedHotel = ({ hotel }) => {
       <img src={hotel.image[0]} alt={hotel.name} className="carousel-image" />
       <p>{hotel.name}</p>
       <p>{hotel.description}</p>
+      <div>
+        <a href="#">
+          <RiFacebookCircleLine />
+        </a>
+        <a href="#">
+          <TiSocialTwitterCircular />
+        </a>
+        <a href="#">
+          <RiInstagramLine />
+        </a>
+      </div>
     </div>
   );
 };
