1bd2585ae83ad9116225c81d61c3eb401c6f6337
Alexr16 <j.alex-1996@hotmail.com>
2022-08-16 12:36:59 -0500

    Refactor routes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.scss';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './pages/HomePage';
+// import HomePage from './pages/HomePage';
 import NavBar from './components/Navbar';
 import { fetchMostRecentHotels } from './redux/MostRecent/MostRecent';
 import SignUp from './components/SignUp';
@@ -22,12 +22,12 @@ const App = () => {
     <Router>
       <NavBar />
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/featured" element={<FeaturedPage />} />
+        {/* <Route path="/" element={<HomePage />} /> */}
+        <Route path="/" element={<FeaturedPage />} />
         <Route path="sign-up" element={<SignUp />} />
         <Route path="log-in" element={<Login />} />
         <Route path="hotel" element={<HotelPage />} />
-        <Route path="/hotel-details" element={<DetailsPage />} />
+        <Route path="/hotel/:name" element={<DetailsPage />} />
       </Routes>
     </Router>
   );
