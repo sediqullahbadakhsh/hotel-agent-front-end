30d9ea7d6340c87f0fd23c1b4572e9e40c980e07
Alexr16 <j.alex-1996@hotmail.com>
2022-08-17 13:07:33 -0500

    Add tests for FeaturedPage and Redux store

---
+++ src/__mocks__/mostRecent.js
@@ -0,0 +1,19 @@
+const mostRecent = {
+  MostRecent: [
+    {
+      id: 11,
+      name: 'Carlton City Hotel Singapore',
+      description: 'Situated in the historical district of Tanjong Pag…el boasts an outdoor swimming pool and a sundeck.',
+      cost: 211,
+      address: '1 Gopeng Street, Chinatown, 078862 Singapore, Singapore',
+      image: [
+        'https://live.staticflickr.com/7880/47534802271_5827731624_z.jpg',
+        'https://live.staticflickr.com/4650/25121215267_cafa74d03b_z.jpg',
+        'https://live.staticflickr.com/1630/25725601291_88972c0319_z.jpg',
+
+      ],
+    },
+  ],
+};
+
+export default mostRecent;
