a3e40803c250b1a7ecdb6756a42a7814659e2c0c
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-08-17 21:35:29 +0430

    remove unneccessary code from hotel page

--- src/pages/HotelPage.jsx
+++ src/pages/HotelPage.jsx
@@ -1,40 +1,19 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import HotelList from '../components/HotelList';
-import { viewHotel, deleteHotel } from '../redux/Hotel/Hotel';
 
 const HomePage = () => {
-  const dispatch = useDispatch();
   const { data } = useSelector((state) => state.Hotel);
-  return (
-    <div className="App">
-      <button
-        type="button"
-        className="microphone"
-        label="microphone"
-        onClick={() => dispatch(viewHotel())}
-      >
-        View Hotel
-      </button>
-      <button
-        type="button"
-        className="microphone"
-        label="microphone"
-        onClick={() => dispatch(deleteHotel())}
-      >
-        Delete Hotel
-      </button>
 
-      <div className="featured-container">
-        <div className="featured-heading">
-          <h1>Featured Hotels</h1>
-          <p>Please select a Hotel</p>
-        </div>
-        <div className="hotel-list">
-          {data.map((hotel) => (
-            <HotelList hotel={hotel} key={hotel.id} />
-          ))}
-        </div>
+  return (
+    <div className="featured-container">
+      <div className="featured-heading">
+        <h1>Hotel List</h1>
+      </div>
+      <div className="hotel-list">
+        {data.map((hotel) => (
+          <HotelList hotel={hotel} key={hotel.id} />
+        ))}
       </div>
     </div>
   );
