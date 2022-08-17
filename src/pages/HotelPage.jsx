69c32aec57619766d59ec49144ea9d0671913a34
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-08-17 16:27:38 +0430

    fix style issues

--- src/pages/HotelPage.jsx
+++ src/pages/HotelPage.jsx
@@ -1,21 +1,13 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import HotelList from '../components/HotelList';
-import { addHotel, viewHotel, deleteHotel } from '../redux/Hotel/Hotel';
+import { viewHotel, deleteHotel } from '../redux/Hotel/Hotel';
 
 const HomePage = () => {
   const dispatch = useDispatch();
   const { data } = useSelector((state) => state.Hotel);
   return (
     <div className="App">
-      <button
-        type="button"
-        className="microphone"
-        label="microphone"
-        onClick={() => dispatch(addHotel())}
-      >
-        Add Hotel
-      </button>
       <button
         type="button"
         className="microphone"
@@ -33,14 +25,16 @@ const HomePage = () => {
         Delete Hotel
       </button>
 
-      <div className="featured-container hotel-list">
+      <div className="featured-container">
         <div className="featured-heading">
           <h1>Featured Hotels</h1>
           <p>Please select a Hotel</p>
         </div>
-        {data.map((hotel) => (
-          <HotelList hotel={hotel} key={hotel.id} />
-        ))}
+        <div className="hotel-list">
+          {data.map((hotel) => (
+            <HotelList hotel={hotel} key={hotel.id} />
+          ))}
+        </div>
       </div>
     </div>
   );
