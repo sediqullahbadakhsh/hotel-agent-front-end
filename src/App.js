7892b4ca26e2fb24aa841e33ecc0ad0a13cdd769
ahmedadel56 <ahmdadl563@gmail.com>
2022-08-17 23:25:31 +0200

    Fix linter errors

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ import FeaturedPage from './pages/FeaturedPage';
 import ErrorPage from './pages/ErrorPage';
 import ReservePage from './pages/ReservePage';
 import AddHotel from './pages/AddHotelPage';
-import ReserveDefault from './components/ReserveDefault';
 import { listHotel } from './redux/Hotel/Hotel';
 import ReserveDefault from './components/ReserveDefault';
 
