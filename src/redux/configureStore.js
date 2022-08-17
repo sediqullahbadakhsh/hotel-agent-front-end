be786368394cf303d26fe3a09314a6a220d1e515
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-08-18 02:27:15 +0430

    configure Reservation store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -4,11 +4,13 @@ import logger from 'redux-logger';
 import mostRecentReducer from './MostRecent/MostRecent';
 import UserReducer from './User/User';
 import HotelReducer from './Hotel/Hotel';
+import ReservationReducer from './reservations/reservation';
 
 const rootReducer = combineReducers({
   MostRecent: mostRecentReducer,
   User: UserReducer,
   Hotel: HotelReducer,
+  Reservation: ReservationReducer,
 });
 
 const store = createStore(rootReducer, applyMiddleware(thunk, logger));
