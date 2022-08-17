3124e21daf9da9048d8b854fa7aebff9189770ad
ahmedadel56 <ahmdadl563@gmail.com>
2022-08-17 22:35:37 +0200

    Work on list of reservaion of the user and delete reservation functionality

--- src/redux/reservations/reservation.js
+++ src/redux/reservations/reservation.js
@@ -1,44 +1,41 @@
 // const LIST_HOTEL = 'HotelAgentFrontEnd/Hotel/LIST_HOTEL';
 const ADD_RESERVATION = 'HotelAgentFrontEnd/reservations/ADD_RESERVATION';
-// const VIEW_HOTEL = 'HotelAgentFrontEnd/Hotel/VIEW_HOTEL';
-// const DELETE_HOTEL = 'HotelAgentFrontEnd/Hotel/DELETE_HOTEL';
+const VIEW_RESERVATIONS = 'HotelAgentFrontEnd/reservations/VIEW_RESERVATIONS';
+const DELETE_RESERVATION = 'HotelAgentFrontEnd/Hotel/DELETE_RESERVATION';
 
 const initialState = { status: 'No Data', data: [] };
 
-// export const dataSucces = (hotel) => ({
-//   type: LIST_HOTEL,
-//   hotel,
-// });
+
 export const addReservation = (reservation) => ({
   type: ADD_RESERVATION,
   reservation,
 });
-// export const viewHotelSucces = (hotel) => ({
-//   type: VIEW_HOTEL,
-//   hotel,
-// });
-// export const deleteHotelSucces = (hotel) => ({
-//   type: DELETE_HOTEL,
-//   hotel,
-// });
+export const viewReservetions = (reservations) => ({
+  type: VIEW_RESERVATIONS,
+  reservations,
+});
+export const deleteReservation = (reservation) => ({
+  type: DELETE_RESERVATION,
+  reservation,
+});
 
-// export const listHotel = () => async (dispatch) => {
-//   fetch('http://localhost:3000/v1/users/2/hotels', {
-//     method: 'GET',
-//     headers: {
-//       Accept: 'application/json',
-//       'Content-Type': 'application/json',
-//       Authorization: `Bearer ${localStorage.getItem('token')}`,
-//     },
-//   })
-//     .then((data) => data.json())
-//     .then((data) => {
-//       dispatch(dataSucces(data));
-//     })
-//     .catch((error) => {
-//       throw error;
-//     });
-// };
+export const listReservations = () => async (dispatch) => {
+  fetch(`http://localhost:3000/v1/users/${localStorage.getItem('userId')}/reservations`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+    },
+  })
+    .then((data) => data.json())
+    .then((data) => {
+      dispatch(viewReservetions(data));
+    })
+    .catch((error) => {
+      throw error;
+    });
+};
 
 export const addHotelReservation = (data) => async (dispatch) => {
   fetch(`http://127.0.0.1:3000/v1/users/${localStorage.getItem('userId')}/reservations`, {
@@ -65,51 +62,36 @@ export const addHotelReservation = (data) => async (dispatch) => {
       throw error;
     });
 };
-// export const viewHotel = () => async (dispatch) => {
-//   fetch('http://127.0.0.1:3000/v1/users/2/hotels/2', {
-//     method: 'GET',
-//     headers: {
-//       Accept: 'application/json',
-//       'Content-Type': 'application/json',
-//       Authorization: `Bearer ${localStorage.getItem('token')}`,
-//     },
-//   })
-//     .then((data) => data.json())
-//     .then((data) => {
-//       dispatch(viewHotelSucces(data));
-//     })
-//     .catch((error) => {
-//       throw error;
-//     });
-// };
-// export const deleteHotel = () => async (dispatch) => {
-//   fetch('http://127.0.0.1:3000/v1/users/2/hotels/3', {
-//     method: 'DELETE',
-//     headers: {
-//       Accept: 'application/json',
-//       'Content-Type': 'application/json',
-//       Authorization: `Bearer ${localStorage.getItem('token')}`,
-//     },
-//   })
-//     .then((data) => data.json())
-//     .then((data) => {
-//       dispatch(deleteHotelSucces(data));
-//     })
-//     .catch((error) => {
-//       throw error;
-//     });
-// };
+
+export const deleteHotelReservation = () => async (dispatch) => {
+    // attention please
+    // You neeed to spacify reservation id in the link
+
+  fetch(`http://127.0.0.1:3000/v1/users/${localStorage.getItem('userId')}/reservations/3`, {
+    method: 'DELETE',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+    },
+  })
+    .then((data) => data.json())
+    .then((data) => {
+      dispatch(deleteReservation(data));
+    })
+    .catch((error) => {
+      throw error;
+    });
+};
 
 const ReservationReducer = (state = initialState, action) => {
   switch (action.type) {
-    // case LIST_HOTEL:
-    //   return { data: action.hotel, status: 'successfully loaded' };
     case ADD_RESERVATION:
       return { data: action.reservation, status: 'Reservation successfully added' };
-    // case VIEW_HOTEL:
-    //   return { data: action.hotel, status: 'Hotel successfully loaded' };
-    // case DELETE_HOTEL:
-    //   return { data: action.hotel, status: 'Hotel successfully Deleted' };
+    case VIEW_RESERVATIONS:
+      return { data: action.reservations, status: 'Reservations successfully loaded' };
+    case DELETE_HOTEL:
+      return { data: action.hotel, status: 'Reservation  successfully Deleted' };
     default:
       return state;
   }
