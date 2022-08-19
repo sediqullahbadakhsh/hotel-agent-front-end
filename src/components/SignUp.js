2bb1cad50165cf27e4dcf3ef73066f4af3b1a351
ahmedadel56 <ahmdadl563@gmail.com>
2022-08-19 11:19:25 +0200

    Add validations to sign up form

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -9,7 +9,7 @@ import { createUser } from '../redux/User/User'
 export default function SignUp() {
   const navigate = useNavigate()
   const dispatch = useDispatch();
-  const { register, handleSubmit } = useForm();
+  const { register,formState: { errors }, handleSubmit } = useForm();
   const onSubmit = (data) => (dispatch(createUser(data)) ? navigate('/log-in') : null) ;
 
   return (
@@ -22,13 +22,19 @@ export default function SignUp() {
       </div>
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="login-form">
-          <input {...register('name')} placeholder="👤  name" />
+          <input {...register('name')} required minLength={3} maxLength ={20} placeholder="👤  name" />
         </div>
         <div className="login-form">
-          <input {...register('email')} placeholder ="✉️  email" />
+          <input {...register('email',{
+          required: "Email address is required",
+          pattern: {
+            value: /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+            message: "Invalid email address"}
+          })} placeholder ="✉️  email" />
+          {errors.email && <p>{errors.email.message}</p>}
         </div>
         <div className="login-form">
-          <input {...register('password')} placeholder="🔒  password" />
+          <input {...register('password')}  required minLength={6} placeholder="🔒  password" />
         </div>
         <div className="login-btn">
           <input type="submit" value="SIGN UP"/>
