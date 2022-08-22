e3a2a467ba1bae69251230d51149d1849dc7a07b
Alexr16 <j.alex-1996@hotmail.com>
2022-08-22 15:03:05 -0500

    Fix linter errors

--- src/__tests__/Signup.test.js
+++ src/__tests__/Signup.test.js
@@ -2,7 +2,7 @@ import renderer from 'react-test-renderer';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import store from '../redux/configureStore';
-import Signup from '../components/Signup';
+import Signup from '../components/SignUp';
 
 it('Check if Sign up page renders correctly', () => {
   const tree = renderer.create(
